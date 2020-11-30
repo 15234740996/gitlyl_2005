246bbe21e098152b724f540f29badafc245a750c
liuyl <1095816743@qq.com>
2020-11-30 19:11:51 +0800

    测试类添加一个主函数

--- src/测试/爪哇/com/百芝/Gitlyl2005ApplicationTests.java
+++ src/测试/爪哇/com/百芝/Gitlyl2005ApplicationTests.java
@@ -9,6 +9,11 @@ class Gitlyl2005ApplicationTests {
     @Test
     void contextLoads() {
            int aaa=10;
+           System.out.println(aaa);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-----");
     }
 
 }
